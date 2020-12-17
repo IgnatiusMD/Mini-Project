6d83fbd19bc2eb2e525ac3d23de8ff97e3fcc9a1
IgnatiusMD <michaeldinata33@gmail.com>
2020-12-17 20:01:11 +0700

    Final

--- js/form.js
+++ js/form.js
@@ -3,15 +3,22 @@ const email = document.getElementById('email')
 const phone = document.getElementById('phone')
 const isi = document.getElementById('isi')
 const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e)=>{
-    let messages = []
-    if(nama.value === '' || nama.value === null){
-        messages.push('This Field cannot be empty!')
+function validate(){
+    if(nama.value==""){
+        alert("Name Field Cannot be empty")
+        return false;
     }
-    if(messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+    if(email.value==""){
+        alert("Email Field Cannot be empty")
+        return false;
     }
-});
+    if(phone.value==""){
+        alert("Phone Field Cannot be empty")
+        return false;
+    }
+    if(isi.value==""){
+        alert("Message Field Cannot be empty")
+        return false;
+    }
+}
